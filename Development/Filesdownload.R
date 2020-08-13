023efb89ab1ba27bd5d179c0886962f9a4cf29d1
vitergo <38817955+vitergo@users.noreply.github.com>
2020-08-13 17:35:16 -0500

    testing with members list
    testing with members list

--- Development/Filesdownload.R
+++ Development/Filesdownload.R
@@ -2,8 +2,8 @@
 # Get direct links from google drive: https://sites.google.com/site/gdocs2direct/
 
 library(RCurl)
-URL <- 'https://drive.google.com/uc?export=download&id=1IxV_xQtSnWD6a8SbFlsNYkXCPBDEQyfa'
-x <- getURL(URL)
 
+
+URL <- 'https://drive.google.com/uc?export=download&id=1IxV_xQtSnWD6a8SbFlsNYkXCPBDEQyfa'
 
 download.file(URL,destfile="bio_VTG.Rmd",method="libcurl")
